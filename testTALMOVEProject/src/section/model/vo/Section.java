a3e9c9e6c48f762bb954be545958fcb7270aea75
zwoo201537028 <45746220+zwoo201537028@users.noreply.github.com>
2019-09-25 09:57:15 +0900

    courseDetail
    courseDetail

--- testTALMOVEProject/src/section/model/vo/Section.java
+++ testTALMOVEProject/src/section/model/vo/Section.java
@@ -1,8 +1,45 @@
 package section.model.vo;
 
-public class Section {
+import java.io.Serializable;
 
+public class Section implements Serializable{
+	private static final long serialVersionUID = 2000L;
+	
 	private int sectionNo;
 	private int courseNo;
 	private String sectionName;
+	public Section(int sectionNo, int courseNo, String sectionName) {
+		super();
+		this.sectionNo = sectionNo;
+		this.courseNo = courseNo;
+		this.sectionName = sectionName;
+	}
+	
+	public Section() {}
+
+	public int getSectionNo() {
+		return sectionNo;
+	}
+
+	public void setSectionNo(int sectionNo) {
+		this.sectionNo = sectionNo;
+	}
+
+	public int getCourseNo() {
+		return courseNo;
+	}
+
+	public void setCourseNo(int courseNo) {
+		this.courseNo = courseNo;
+	}
+
+	public String getSectionName() {
+		return sectionName;
+	}
+
+	public void setSectionName(String sectionName) {
+		this.sectionName = sectionName;
+	}
+	
+	
 }
