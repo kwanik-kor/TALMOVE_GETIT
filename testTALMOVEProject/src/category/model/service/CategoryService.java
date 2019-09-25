127681ed4461e3265e68584282dc67e6e337597c
KwanIkJang <44356083+rhksdlr134@users.noreply.github.com>
2019-09-25 10:55:32 +0900

    ㅇㅇ바꿔벌임
    ㅇㅇ바꿔벌임

--- testTALMOVEProject/src/category/model/service/CategoryService.java
+++ testTALMOVEProject/src/category/model/service/CategoryService.java
@@ -125,6 +125,13 @@ public class CategoryService {
 		return tlist;
 	}
 
+	public ArrayList<Category> getAllCategoryList() {
+		Connection conn = getConnection();
+		ArrayList<Category> clist = cDao.getAllCategoryList(conn);
+		close(conn);
+		return clist;
+	}
+
 }
 
 
