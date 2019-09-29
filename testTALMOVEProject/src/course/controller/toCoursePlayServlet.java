fb0dd590dee8090bac1044053a28fb79a55b7dd1
KwanIkJang <44356083+rhksdlr134@users.noreply.github.com>
2019-09-29 23:47:28 +0900

    ㅁㄴㅇ

---
+++ testTALMOVEProject/src/course/controller/toCoursePlayServlet.java
@@ -0,0 +1,38 @@
+package course.controller;
+
+import java.io.IOException;
+import java.util.ArrayList;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import course.model.service.CourseService;
+import course.model.vo.Course;
+import lecture.model.service.LectureService;
+import lecture.model.vo.Lecture;
+import section.model.vo.Section;
+
+@WebServlet("/cplay")
+public class toCoursePlayServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    public toCoursePlayServlet() {
+        super();
+    }
+
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		int courseNo = Integer.parseInt(request.getParameter("courseNo"));
+		CourseService courseService = new CourseService();
+		Course c = courseService.getCourse(courseNo);
+		ArrayList<Section> slist = courseService.SectionList(courseNo);
+		
+	}
+
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		doGet(request, response);
+	}
+
+}
